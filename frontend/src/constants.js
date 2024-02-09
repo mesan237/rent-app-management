ac850254709d1f29610b3757166f14c23585d71a
mesan237 <negokamgaing@gmail.com>
2024-02-09 20:17:08 +0100

    editing the URL for APIs

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
-// export const BASE_URL =
-//   process.env.NODE_ENV === "development" ? "http//localhost:5000" : "";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http//localhost:5000" : "";
+// export const BASE_URL = "http://localhost:5000";
 // export const BASE_URL = "";
 export const LOCATAIRES_URL = "/api/locataires";
 export const DEPENSES_URL = "/api/depenses";
