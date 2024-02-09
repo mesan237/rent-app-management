a53ea7afcecd5b282a882c889f04f95355e51268
mesan237 <negokamgaing@gmail.com>
2024-02-09 19:41:50 +0100

    edit production block

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ cron.schedule("0 0 28 * *", () => {
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
